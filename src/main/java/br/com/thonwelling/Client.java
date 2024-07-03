d0fac15bdb9b211b9c0f1a6f6c018ec019b432e4
Thonwelling <thonwellingdani13@gmail.com>
2024-07-03 00:05:33 -0300

    Structuring Client Class

--- src/main/java/br/com/thonwelling/Client.java
+++ src/main/java/br/com/thonwelling/Client.java
@@ -1,4 +1,42 @@
 package br.com.thonwelling;
 
+import java.time.LocalDateTime;
+
 public class Client {
+    private String firstName;
+    private String lastName;
+    private String documentId;
+    private LocalDateTime birthDay;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    public LocalDateTime getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDateTime birthDay) {
+        this.birthDay = birthDay;
+    }
 }
