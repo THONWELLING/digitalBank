363fa7e304784b58a4ab114f87449fbe1ee3497d
Thonwelling <thonwellingdani13@gmail.com>
2024-07-03 01:09:34 -0300

    Implementing The Main Class

--- src/main/java/br/com/thonwelling/Main.java
+++ src/main/java/br/com/thonwelling/Main.java
@@ -1,11 +1,26 @@
 package br.com.thonwelling;
 
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome Thonwelling!");
-         for (int i = 1; i <= 5; i++) {
-           System.out.println("i = " + i);
-        }
+        Client Thon = new Client();
+        Thon.setFirstName("Thonwelling");
+        Thon.setLastName("Dani");
+        Thon.setBirthDay(LocalDateTime.now().minusYears(38));
+        Thon.setDocumentId("084.422.828-15");
+
+        System.out.printf("Hello and welcome %s !", Thon.getFirstName());
+
+        Account currentAccount = new CurrentAccount(Thon);
+        Account savingAccount  = new SavingsAccount(Thon);
+
+        currentAccount.depositMoney(5000000);
+        currentAccount.transferMoney(500000, savingAccount);
+
+        currentAccount.printStatment();
+        savingAccount.printStatment();
+
     }
 }
