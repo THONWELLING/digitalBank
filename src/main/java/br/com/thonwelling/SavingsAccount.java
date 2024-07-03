d8646c2fd3ee827675c7b2d86a2925144c1f2cf1
Thonwelling <thonwellingdani13@gmail.com>
2024-07-03 00:40:58 -0300

    Creating  SavingsAccount

--- src/main/java/br/com/thonwelling/SavingsAccount.java
+++ src/main/java/br/com/thonwelling/SavingsAccount.java
@@ -1,4 +1,13 @@
 package br.com.thonwelling;
 
-public class SavingsAccount {
+public class SavingsAccount extends Account {
+    public SavingsAccount(Client client) {
+        super(client);
+    }
+
+    @Override
+    public void printStatment(){
+        System.out.println("---> Savings Account Statment <---");
+        super.printCommonInfos();
+    }
 }
